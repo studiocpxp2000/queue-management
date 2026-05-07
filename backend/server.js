5fdaec847e6ebd298d3d59b12ab689dc3c131762
Abhishek Choudhary <abhich2109@gmail.com>
2026-05-07 14:26:04 +0530

    Fix Express 5 wildcard routing crash

--- backend/server.js
+++ backend/server.js
@@ -144,7 +144,7 @@ app.post('/api/admin/settings', (req, res) => {
 
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
